5be417ce5ad01a25b481ebfdf9797982cbe083c0
GrahamMueller(Desktop) <>
2023-07-14 18:25:38 -0700

    Updated path for custom modules

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-scons platform=windows arch=x86_64 vsproj=yes target=editor dev_mode=yes dev_build=yes custom_modules=../godot_custom_modules 2>&1 | tee build.sh.log
+scons platform=windows arch=x86_64 vsproj=yes target=editor dev_mode=yes dev_build=yes custom_modules=../DungeonARPGGame/engine_custom_modules 2>&1 | tee build.sh.log
